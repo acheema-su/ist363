eb768a9f72a181d68029488a16130e8c195e79bb
abdur cheema <acheema@syr.edu>
2023-09-14 18:20:23 -0400

    Attendance 09/14/2023

--- starbucks/js/main.js
+++ starbucks/js/main.js
@@ -37,7 +37,7 @@ closeBtn.addEventListener("click", function(){
 const coffeeName = "Mocha";
 const coffeeHeadline = document.createElement("h2");
 coffeeHeadline.textContent = coffeeName;
-document.body.appendChild(coffeeHeadline);
+//document.body.appendChild(coffeeHeadline);
 
 
 //objects
@@ -51,21 +51,92 @@ const coffee = {
 //console.log(coffee.description);
 const coffeeObjHeadline = document.createElement("h2");
 coffeeObjHeadline.textContent = coffee.name;
-document.body.appendChild(coffeeObjHeadline);
+//document.body.appendChild(coffeeObjHeadline);
 
-const coffees = ["Mocha", "Latte", "Espresso"];
+
+const coffees = [
+   {
+      name: "Mocha",
+      price: 3.99,
+      description: "This is a delicious cup of coffee",
+      image: {
+         fileName: "coffee.jpg",
+         width: 630,
+         height: 630,
+         altText: "A cup of coffee."
+      }
+}
+,
+{
+
+   name: "Latte",
+   price: 3.99,
+   description: "This is a delicious cup of coffee",
+   image: {
+      fileName: "coffee.jpg",
+      width: 630,
+      height: 630,
+      altText: "A cup of coffee."
+   }
+}
+,
+{
+   name: "Espresso",
+   price: 3.99,
+   description: "This is a delicious cup of coffee",
+   image: {
+      fileName: "coffee.jpg",
+      width: 630,
+      height: 630,
+      altText: "A cup of coffee."
+   }
+}
+
+];
+
+
 //console.log(coffees[0]);
 
 
 coffees.forEach(function(coffee) {
-   console.log(coffee)
+   
+   //const coffeeTitle = coffee;
+   //const coffeeSecondaryHeadline = document.createElement("h2");
+  // coffeeSecondaryHeadline.textContent = coffeeTitle;
+//document.body.appendChild(coffeeSecondaryHeadline);
+
+
+//1. deconstruct the coffee object
+const {name, price, description, image} = coffee;
+
+
+//2.create the html elements
+const coffeeArticle = document.createElement("article")
+
+const coffeeImage = document.createElement("img")
+coffeeImage.src = `images/${image.fileName}`;
+
+const coffeeName = document.createElement("h2");
+coffeeImage.textContent = name;
+coffeeImage.width = image.width;
+coffeeImage.height = image.height;
+coffeeImage.alt = image.altText;
+
+
+const coffeePrice = document.createElement("h3");
+coffeePrice.textContent = price;
+
+const coffeeDescription = document.createElement("p");
+coffeeDescription.textContent = description;
+
+
+coffeeArticle.appendChild(coffeeImage);
+coffeeArticle.appendChild(coffeeName);
+coffeeArticle.appendChild(coffeePrice);
+coffeeArticle.appendChild(coffeeDescription);
+
+
+document.body.appendChild(coffeeArticle);
+
 
 });
-
-
-
-
-
-
-
-
